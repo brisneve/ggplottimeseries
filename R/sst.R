8f3053fd36d84c0211e0dc7f5b4471351c16cdc8
brisneve <bedullantes@up.edu.ph>
2019-02-15 10:30:32 +0000

    updated

--- R/sst.R
+++ R/sst.R
@@ -2,9 +2,6 @@
 #'
 #' Daily sea surface temperature data downloaded from Giovanni
 #'
-#' @name sst
-#' @docType data
-#'
 #' @format a sample dataframe with 2142 observations on the following 2 variables.
 #' \describe{
 #'   \item{\code{date}}{a date vector of the time of the observation}
