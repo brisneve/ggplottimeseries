f293822f599063b4d0bebe7e1904302929312367
brisneve <bedullantes@up.edu.ph>
2019-02-15 11:18:41 +0000

    updated

--- R/sst.R
+++ R/sst.R
@@ -1,12 +1,15 @@
 #' Sea surface temperature data
 #'
 #' Daily sea surface temperature data downloaded from Giovanni
-#'
-#' @format a sample dataframe with 2142 observations on the following 2 variables.
+#' @name sst
+#' @docType data
+#' @format a sample dataframe with 2142 observations on the following variables.
 #' \describe{
 #'   \item{\code{date}}{a date vector of the time of the observation}
 #'   \item{\code{sst}}{a numeric vector}}
 #'
 #' @author Brisneve Edullantes
 
-"sst"
+library(readr)
+sst <- read_csv("data/sst.csv")
+usethis::use_data(sst, overwrite = TRUE)
