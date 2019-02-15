f6d07df31af74b24936c52687173584acde25c24
brisneve <bedullantes@up.edu.ph>
2019-02-15 10:24:10 +0000

    updated

--- R/sst.R
+++ R/sst.R
@@ -5,16 +5,12 @@
 #' @name sst
 #' @docType data
 #'
-#' @format data frame with 2142 observations on the following 2 variables.
+#' @format a sample dataframe with 2142 observations on the following 2 variables.
 #' \describe{
 #'   \item{\code{date}}{a date vector of the time of the observation}
 #'   \item{\code{sst}}{a numeric vector}}
 #'
 #' @author Brisneve Edullantes
-#'
-#' @references
-#'
-#' J. G. Acker and G. Leptoukh, “Online Analysis Enhances Use of NASA Earth Science Data”, Eos, Trans. AGU, Vol. 88, No. 2 (9 January 2007), pages 14 and 17.
 #'
 #' @source \url{to_be_added}
 "sst"
